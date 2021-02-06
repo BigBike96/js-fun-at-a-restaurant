62d13777ad594d473521a8ea265196c296832e2c
Jim Charnesky <mijnesky@hotmail.com>
2021-02-06 10:17:51 -0700

    Add working order functions to pass tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,35 @@
+function takeOrder(obj, array) {
+  if (array.length < 3)
+  array.push(obj);
+}
+
+function refundOrder(number, array) {
+  array.splice(number - 1, 1)
+}
+
+function listItems(array) {
+  var string = [];
+  for (var i = 0; i < array.length; i++) {
+    string.push(array[i].item);
+    if (string.length === array.length) {
+      return `${string[0]}, ${string[1]}, ${string[2]}`;
+    }
+  }
+}
+
+function searchOrder(arrayOfOrders, itemOnList) {
+  var orders = [];
+  for (var i = 0; i < arrayOfOrders.length; i++) {
+    orders.push(arrayOfOrders[i].item);
+    if (orders.length === arrayOfOrders.length) {
+      return orders.includes(itemOnList);
+    }
+  }
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }

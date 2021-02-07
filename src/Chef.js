828be7603d35e156a23e02c1d22e1afc94848645
Jim Charnesky <mijnesky@hotmail.com>
2021-02-06 17:01:41 -0700

    Add a class and methods to pass tests

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,21 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, eatery) {
+    this.name = name;
+    this.restaurant = eatery;
+  }
+  greetCustomer(name, morning) {
+    if (morning === true) {
+      return `Good morning, ${name}!`
+    } else {
+      return `Hello, ${name}!`;
+    }
+  }
+  checkForFood(foodItemCheck) {
+    if (foodItemCheck.name === "Quiche" || foodItemCheck.name === "Kouign Amann") {
+      return `Sorry, we aren't serving ${foodItemCheck.name} today.`
+    } else {
+      return `Yes, we're serving ${foodItemCheck.name} today!`
+    }
   }
 }
 
